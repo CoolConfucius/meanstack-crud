12104a285aa7d2256182baa5756d37123ee3af1f
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-15 09:49:46 -0800

    third commit

--- public/js/module.js
+++ public/js/module.js
@@ -10,3 +10,35 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/'); 
 });
+
+
+app.directive('myTable', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      list: '=',
+      sorttext: '='
+    },
+    controller: 'myTableCtrl', 
+    templateUrl: '/html/templates/myTable.html'
+  };
+});
+
+app.filter('titlecase', function() {
+  return function(input) {
+    if (typeof input !== 'string') return input;
+    return input[0].toUpperCase() + input.slice(1).toLowerCase(); 
+  };
+}); 
+
+app.controller('myTableCtrl', function($scope){
+  console.log("myTable ctrl");
+  $scope.sort = function(key){
+    console.log("sort!");
+    if ($scope.sorttext === key) {
+      $scope.sorttext = '-'+key;   
+    } else {
+      $scope.sorttext = key; 
+    }
+  }
+})
